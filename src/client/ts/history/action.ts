732a3be710fdc482cdcf0fdff1e80ef6e36a6191
Guillaume <108530065+baldurstod@users.noreply.github.com>
2026-09-10 23:22:06 +0200

    Added overloads for action do()

--- src/client/ts/history/action.ts
+++ src/client/ts/history/action.ts
@@ -1,4 +1,10 @@
+import { SfmClip } from '../model/clips/clip';
+import { SfmFilmClip } from '../model/clips/filmclip';
+import { SfmEntity } from '../model/entity';
 import { SfmNode } from '../model/node';
+import { SfmTrack } from '../model/track';
+import { SfmTrackGroup } from '../model/trackgroup';
+import { Serializable } from '../serialize/serializable';
 
 export interface Undoable {
 	do(command: Command): boolean;
@@ -38,9 +44,21 @@ export class Action {
 	#operations: Command[] = [];
 	#committed = false;
 
-	//do(element: SfmNode, command: 'add-child', params: SfmNode): boolean;
-	//do(element: SfmNode, command: 'set-entity', params: SfmEntity): boolean;
-	//do(element: SfmNode, command: 'set-parent', params: SfmNode): boolean;
+	do(element: Serializable, command: 'set-name', params: string): boolean;
+	do(element: SfmClip, command: 'set-end', params: number): boolean;
+	do(element: SfmClip, command: 'set-start', params: number): boolean;
+	do(element: SfmFilmClip, command: 'add-selected-clip', params: SfmClip): boolean;
+	do(element: SfmFilmClip, command: 'add-track-group', params: SfmTrackGroup): boolean;
+	do(element: SfmFilmClip, command: 'delete-track-group', params: SfmTrackGroup): boolean;
+	do(element: SfmFilmClip, command: 'set-selected-clip', params: SfmClip): boolean;
+	do(element: SfmNode, command: 'add-child', params: SfmNode): boolean;
+	do(element: SfmNode, command: 'set-entity', params: SfmEntity): boolean;
+	do(element: SfmNode, command: 'set-parent', params: SfmNode): boolean;
+	do(element: SfmTrack, command: 'add-clip', params: SfmClip): boolean;
+	do(element: SfmTrack, command: 'delete-clip', params: SfmClip): boolean;
+	do(element: SfmTrackGroup, command: 'add-track', params: SfmTrack): boolean;
+	do(element: SfmTrackGroup, command: 'delete-track', params: SfmTrack): boolean;
+
 	do(element: Undoable, command: string, params: any): boolean {
 		const operation = new Command(this, element, command, params);
 		const result = element.do(operation);
